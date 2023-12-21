9a82550d8980ed8c6b252faf01a65b24857a2c0d
jlotten <jenna.otten@gmail.com>
2023-12-21 12:59:45 -0800

    Added nav to view record page and redirected login page to correct route

--- frontend/src/components/ViewRecord.tsx
+++ frontend/src/components/ViewRecord.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import ResponsiveAppBar from "./ResponsiveAppBar";
 
 function ViewRecord() {
   return (
     <div>
+      <ResponsiveAppBar />
       <h1>View Record</h1>
+      
     </div>
   );
 }
