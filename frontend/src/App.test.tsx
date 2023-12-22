a6be52151fbdc4ef057d9a7580058389c0850bf5
jlotten <jenna.otten@gmail.com>
2023-12-21 18:07:23 -0800

    Fix tests to reflect actual page content

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,29 +4,24 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import FamilyMemberLog from './components/FamilyMemberLog';
 
-const linkElement = screen.getByText((content, node) => {
-  const hasText = (node) => node.textContent === 'Sign in';
-  const nodeHasText = hasText(node);
-  const childrenDontHaveText = Array.from(node.children).every(
-    (child) => !hasText(child)
+test('renders home page', () => {
+  render(
+    <App />
   );
+  const homeElement = screen.getByText(/Welcome to your vaccine manager/i);
+  expect(homeElement).toBeInTheDocument();
+})
 
-  return nodeHasText && childrenDontHaveText;
-});
-{/*test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Sign in', {exact: true});
-  expect(linkElement).toBeInTheDocument();
-});
-*/}
 
 test('renders family member log page', () => {
   render(
-  <Router> 
-    <FamilyMemberLog />
-  </Router>
+    <Router>
+      <FamilyMemberLog />
+    </Router>
   );
-  const linkElement = screen.getByText(/Family Member Log/i);
 
-  expect(linkElement).toBeInTheDocument();
+
+  const logElement = screen.getByText(/Family Member Log/i);
+
+  expect(logElement).toBeInTheDocument();
 });
